a712397a8859fc56c7542bc5edfc628afd36186a
github username <jstn.mnld@gmail.com>
2017-11-22 13:50:47 -0500

    user schema defined

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("./index").mongoose;
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password_digest: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  oneRepMaxes: {
+    deadlift1RM: Number,
+    benchpress1RM: Number,
+    squat1RM: Number,
+    militaryPress1RM: Number
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
